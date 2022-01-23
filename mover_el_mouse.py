7a2766ec104e693ff6de21b51eb055ed545f022d
migno chrono <mignochrono.nc@gmail.com>
2022-01-23 18:59:10 -0500

    added coments to understand the program, but now it dosent work on VS??

--- mover_el_mouse.py
+++ mover_el_mouse.py
@@ -1,10 +1,10 @@
-import pyautogui
-import time
+import pyautogui #import pyautogui
+import time #import time because if not it goes too fast
 
-print ('starting...')
-banana = 1000
-while banana > 1:
-    mouse=pyautogui.position()
+print ('starting...') #marcador de que el programa inicio bien
+banana = 1000 # a variable so we can limit the time
+while banana > 1: #simple while loop
+    mouse=pyautogui.position() #function to find the position of the mouse on the screen
     print('the mouse its on this place')
     print(mouse)
     banana= banana - 1
